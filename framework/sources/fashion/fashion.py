7d51c880f802ad58ec34a1e6ae0e40d5399ba88d
Atom <blue.atakan.14@gmail.com>
2020-06-04 03:47:50 +0300

    last changs

--- framework/sources/fashion/fashion.py
+++ framework/sources/fashion/fashion.py
@@ -1,31 +1,32 @@
 import tensorflow as tf
 from tensorflow import keras
-import cv2
-
-fashion_mnist = keras.datasets.fashion_mnist
 
 #load fashion_mnist dataset
+fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+#define class names
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-#cv2.imwrite('/home/atom/Desktop/Deep_Learning_Framework/example4.png', train_images[1])
+#preparing train and test datasets
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
-
+#model architecture
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(10)
 ])
 
+#model compile
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
+#model training
 model.fit(train_images, train_labels, epochs=10)
 
+#save model
 model.save("fashion.h5")
